c416f9313c12183a45a8e8b301c7350b15c83e44
FOX0018 <FOX0018@elthamhs.vic.edu.au>
2025-06-20 21:06:54 +1000

    Comments and Naming

--- Cocoa Roots.py
+++ Cocoa Roots.py
@@ -2,32 +2,38 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import pickle
 
+# lists of classes and objects
+# these lists are global
 ingredients = []
 batches = []
 tickets = []
 
-red = "#b3152a"
-orange = "#F6A482"
-dark_orange = "#ed652d"
-light_orange = "#FEDBC7"
-light_blue = "#D1E5F0"
-dark_blue = "#8EC4DE"
+# global colours
+RED = "#b3152a"
+ORANGE = "#F6A482"
+DARK_ORANGE = "#ed652d"
+LIGHT_ORANGE = "#FEDBC7"
+LIGHT_BLUE = "#D1E5F0"
+DARK_BLUE = "#8EC4DE"
+BACKGROUND = "#f5f0ed"
 
 
-
+# -----------------------------------------------------------------------------
+# FUNCTIONALITY
+# -----------------------------------------------------------------------------
 
 class Ingredient:
     def __init__(self, name, weight, source):
-        self.log = []
-        self.batch_id = 1
-        self.name = name
+        self.log = []  # list of every event occurred in ingredient
+        self.ID = 1  # ingredient unique identifier
+        self.name = name  # common name of batch
         self.weight = weight
-        self.source = source
+        self.source = source  # name of ingredient suppler
 
     def reduce_amount(self, amount):
         calc_weight = self.weight - amount
 
-        if calc_weight >= 0:
+        if calc_weight >= 0:  # range check
             self.weight = calc_weight
 
         elif calc_weight < 0:
@@ -36,9 +42,9 @@ class Ingredient:
 
 class Batch:
     def __init__(self, ID):
-        self.log = []
+        self.log = []  # list of every event occurred in batch
         self.total_weight = 0
-        self.ID = ID
+        self.ID = ID  # batch unique identifier
 
     def add_ingredient(self, date_time, ingredient, amount):
         ingredient.reduce_amount(amount)
@@ -81,7 +87,7 @@ class Batch:
 
     def winnowing(self, date_time, weight_reduced):
 
-        if weight_reduced > self.total_weight:
+        if weight_reduced > self.total_weight:  # range check
             raise ValueError("weight_reduced cannot be greater than total weight")
 
         record = {"process": "winnowing",
@@ -91,9 +97,9 @@ class Batch:
 
         self.log.append(record)
 
-    def grinding(self, date_time, fineness):
+    def grinding(self, date_time, fineness):  # fineness in mm
 
-        if fineness < 0:
+        if fineness < 0:  # range check
             raise ValueError("fineness cannot be less than zero")
 
         record = {"process": "grinding",
@@ -158,21 +164,25 @@ class Ticket:
 def load():
     ...
 
+
+# -----------------------------------------------------------------------------
+# GUI INTERFACE
+# -----------------------------------------------------------------------------
+
+# main tkinter setup
 window = tk.Tk()
 
-title = tk.Label(height=4, bg=orange)
+# titlebar
+title = tk.Label(height=4, bg=ORANGE)
 title.grid(row=0, column=0, sticky="ew")
 window.grid_columnconfigure(0, weight=1)
 
+# background
 content = tk.Label(bg="#f5f0ed")
 content.grid(row=1, column=0, sticky="nsew")
 window.grid_rowconfigure(1, weight=1)
 
-item = tk.Label(content, bg=light_blue)
+item = tk.Label(content, bg=LIGHT_BLUE)
 item.pack()
 
-
-
-
 window.mainloop()
-
